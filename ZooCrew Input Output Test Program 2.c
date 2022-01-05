c724c8ed667eead4451a326dac4bd2ec4b2f7734
HanBei543 <95715004+HanBei543@users.noreply.github.com>
2022-01-05 11:49:17 -0800

    Update ZooCrew Input Output Test Program 2.c

--- ZooCrew Input Output Test Program 2.c
+++ ZooCrew Input Output Test Program 2.c
@@ -10,7 +10,7 @@
 #pragma config(Motor,  port3,           leftMotor,     tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port9,           servo,         tmotorServoStandard, openLoop)
 /*
-  Project Title: Zoo Crew R/L Motor Servo Program 1
+  Project Title: Zoo Crew Inputs and Outputs Check Program 2
   Team Members: Hannan Beiken, Adrian portillo
   Date: 12/9/21
   Section: 2
@@ -19,20 +19,35 @@
   Task Description: Verify line follower function and Potentiometer function
 
 
-  Pseudocode: When the potentiometer gear is turned all the way to the left the right motor stops turning, when the potentiometer gear is in the middle it spins to the right,
-  when the potentiometer gear is turned to the left the right motor will turn to the left and turn slower. Dark objects cause the motor to move to the left and the light objects slow or stop the
-  motor.
-
+  Pseudocode: Speed of right motor is proportional to value of potentiometer (Max motor speed when potentiometer at max value).
+  						Turn on the motor with the speed defined above.
+  						
+  						If line follower detects line or dark object,
+  							start left motor
+  						Else
+  							stop left motor
 */
 
 task main()
-{                                     //Program begins, insert code within curly braces
+{
 	while(true)
 	{
-			float speed = sensorValue[potentiometer] / 32;
+		
+	
+	
+			float speed = SensorValue[potentiometer] / 32;
 			startMotor(rightMotor, speed);
-			float speed2 = sensorValue[lineFollower] / 12;
-			startMotor(leftMotor, speed2);
-
+			
+			if (SensorValue[lineFollower] > 200)
+			{
+				startMotor(leftMotor, 40);
+			}
+			else
+			{
+				stopMotor(leftMotor);
+			}
+			
+			
+			
 	}
 }
